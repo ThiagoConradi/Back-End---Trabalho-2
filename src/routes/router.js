677268a66eee591fee3300d1321aacb334dc3710
THIAGO BUTTNER CONRADI <thiago_conradi@estudante.sc.senai.br>
2024-05-28 19:06:50 -0300

    Alterações de Thiago

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,10 +2,8 @@ const { Router } = require("express");
 const router = new Router();
 
 router.get('/', (req, res) => {
-  res.status(200).render("paginaPrincipal")
+    res.status(200).render("paginaPrincipal")
 })
 
-const produtosRoutes = require('./produtos');
-router.use('/produtos', produtosRoutes);
 
 module.exports = router;
