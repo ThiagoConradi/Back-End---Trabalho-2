dd974e501e212bdeb1ec2eef2ce9aebedd4c2a61
THIAGO BUTTNER CONRADI <thiago_conradi@estudante.sc.senai.br>
2024-07-03 19:18:28 -0300

    Alterações

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,6 +3,8 @@ const router = new Router();
 const Usuario = require("../models/Usuario");
 const jwt = require('jsonwebtoken');
 const UsuarioDAO = require("../models/dao/UsuarioDAO");
+const PostagemDAO = require("../models/dao/PostagemDAO");
+
 
 // Variável para armazenar o usuário logado
 let usuarioLogado;
@@ -73,5 +75,48 @@ router.post('/cadastrar', async(req, res) => {
         res.status(200).render("paginaPrincipal")
     }
 })
+
+// Simulação de posts
+const posts = [{
+        titulo: "Lógica de Programação",
+        curtidas: 5,
+        autor: "Thiago Büttner",
+        horario: "10:00",
+        descricao: "A lógica de programação é a base essencial para qualquer pessoa que deseje se aventurar no mundo da programação de computadores. Para facilitar a compreensão e o aprendizado dessa lógica, muitos iniciantes recorrem a ferramentas e linguagens amigáveis, como o Portugol Studio e a linguagem C.",
+        comentarios: [{ autor: "Eduardo Bolsan", horario: "10:05", texto: "Muito bom" }, { autor: "Ramon Bobella", horario: "10: 10", texto: "Muito proveitoso" }]
+    },
+
+    {
+        titulo: "Arquitetura de Software e Hardware",
+        curtidas: 3,
+        autor: "Pablo Southier",
+        horario: "11:00",
+        descricao: "A arquitetura de software, por outro lado, está relacionada à estrutura lógica e ao design dos programas e aplicativos que são executados em um sistema de computação. Isso envolve a organização de módulos de software, a comunicação entre eles e a maneira como os dados são processados e armazenados. A arquitetura de software desempenha um papel crucial na manutenção, escalabilidade e extensibilidade de sistemas de software. Projetar uma arquitetura de software sólida é fundamental para criar aplicativos robustos e flexíveis. A arquitetura de hardware se refere à estrutura física e ao design dos componentes eletrônicos que compõem um sistema de computação. Isso inclui processadores, memória, dispositivos de armazenamento, placas-mãe, periféricos e outros componentes físicos. A arquitetura de hardware desempenha um papel crítico na determinação da capacidade, desempenho e eficiência de um sistema. Compreender a arquitetura de hardware é fundamental para projetar e dimensionar sistemas de computação eficazes.",
+        comentarios: [{ autor: "Guilherme Faccin", horario: "11:05", texto: "Legal" }]
+    },
+
+    {
+        titulo: "Fundamentos UI/UX",
+        curtidas: 8,
+        autor: "Dionathan Luan de Vargas",
+        horario: "12:00",
+        descricao: "A UI, ou Interface do Usuário, refere-se à camada visual e interativa de um produto digital. Isso inclui elementos como botões, menus, cores, tipografia, layouts e todos os elementos visíveis com os quais os usuários interagem. O objetivo da UI é criar uma aparência atraente e funcional que facilite a navegação e a compreensão do sistema. O design de UI se concentra na estética e na usabilidade dos elementos visuais. A UX, ou Experiência do Usuário, abrange a experiência geral do usuário ao interagir com um produto ou sistema. Ela inclui fatores emocionais, cognitivos e práticos. A UX se preocupa com como os usuários se sentem, pensam e realizam tarefas ao usar um produto. Para criar uma boa UX, é fundamental compreender as necessidades, expectativas e comportamentos dos usuários e projetar o sistema de forma a atender a esses requisitos de forma eficiente e agradável. A Importância da Harmonia entre UI e UX: O sucesso de um produto digital depende da harmonia entre UI e UX. Uma interface bonita, mas confusa, pode frustrar os usuários, assim como uma interface funcional, mas pouco atraente, pode não cativar. Portanto, designers de UI/UX trabalham para criar designs visualmente atraentes que também são intuitivos e fáceis de usar.",
+        comentarios: []
+    }
+];
+
+router.get('/paginaPrincipal', (req, res) => {
+    const idPost = req.query.post;
+    if (idPost) {
+        let post = posts[idPost - 1]
+        if (post) {
+            res.status(200).render('paginaPrincipal', { posts, post });
+        } else {
+            res.status(404).send('Post não encontrado');
+        }
+    } else {
+        res.status(200).render('paginaPrincipal', { posts });
+    }
+});
 
 module.exports = router;
